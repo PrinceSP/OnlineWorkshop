e54f12367f4988424b63581cb02ecd5cb2fc937e
PrinceSP <princepasombaran@gmail.com>
2022-05-19 12:57:19 +0800

    import and export all icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import CarLogo from './carLogo.svg'
+import Logo from './logo.svg'
+import MotoLogo from './motoLogo.svg'
+import SuccessIconn from './successIcon.svg'
+
+export {CarLogo,Logo,MotoLogo,SuccessIconn}
