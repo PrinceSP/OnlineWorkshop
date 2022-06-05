8c1f44ea29f1182249805325226ba5d199d0f2d2
EdsonSoumay <s21810088@student.unklab.ac.id>
2022-06-05 10:42:28 +0800

    page bengkel melapor customer

--- App.js
+++ App.js
@@ -10,15 +10,16 @@ import RegisterBengkelMotor from './src/pages/registerBengkelMotor';
 import HomePageBengkel from './src/pages/homepageBengkel';
 import SplashScreen from './src/pages/splashScreen';
 import PermintaanService from './src/pages/permintaanService';
+import BengkelMelaporCustomer from './src/pages/bengkelMelaporCustomer';
 
 const App = () => {
   return (
     <>
-
       {/* <RegisterBengkelMotor/> */}
       {/* <HomePageBengkel/> */}
-      <PermintaanService/>
+      {/* <PermintaanService/> */}
       {/* <SplashScreen/> */}
+      <BengkelMelaporCustomer/>
     </>
   )
 }
