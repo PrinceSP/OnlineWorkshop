93a31924f9b84fb824d3d17a0e316bd127a94f44
PrinceSP <princepasombaran@gmail.com>
2022-06-23 13:57:42 +0800

    register page

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import ProfileCustomer from './src/pages/profileCustomer/index'
-import ProfileBengkel from './src/pages/profileBengkel'
-import RegisterBengkel from './src/pages/registerBengkel'
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes/index';
-import RegisterBengkelMotor from './src/pages/registerBengkelMotor';
 
 const App = () => {
   return (
@@ -20,7 +16,5 @@ const App = () => {
     </>
   )
 }
-
-const styles = StyleSheet.create({})
 
 export default App
