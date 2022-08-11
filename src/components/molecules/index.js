327e78b26b7279e7963e566145fe3ef2e3abdf09
PrinceSP <princepasombaran@gmail.com>
2022-08-11 18:48:45 +0800

    add new component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,4 +1,7 @@
 import FeedbackForm from './feedbackForm'
 import ModalSuccess from './successModal'
+import DrawerContent from './drawerContent'
+import Header from './header'
+import WorkshopComponent from './workshopComp'
 
-export {FeedbackForm,ModalSuccess}
+export {FeedbackForm,ModalSuccess,DrawerContent,Header,WorkshopComponent}
