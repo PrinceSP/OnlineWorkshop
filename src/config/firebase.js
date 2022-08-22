0e53eab58255a92613adc882308d4f9d436c47e3
PrinceSP <princepasombaran@gmail.com>
2022-08-22 14:45:31 +0800

    add firestore configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBmTi7hqx59B7F6JPXbEovHPzCdTEzHTw0",
   authDomain: "bengkelonline-bf821.firebaseapp.com",
@@ -16,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+export const db = getFirestore(app);
