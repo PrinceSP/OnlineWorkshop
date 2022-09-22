ef3b49b9627920d35a19f211fbd15f786fe2a8de
PrinceSP <princepasombaran@gmail.com>
2022-09-22 12:54:40 +0800

    make image update button

--- src/pages/profileCustomer/index.js
+++ src/pages/profileCustomer/index.js
@@ -1,28 +1,68 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
-import React,{useContext} from 'react'
+import React,{useState,useContext} from 'react'
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native'
 import {Gap} from '../../components'
-import { ArrowLeft } from '../../assets'
+import { ArrowLeft,Camera } from '../../assets'
 import {AuthContext} from '../../config/authContext'
+import {launchImageLibrary} from 'react-native-image-picker'
+import firestore from '@react-native-firebase/firestore'
 
 const ProfileCustomer = ({navigation}) => {
   const {user:currentUser} = useContext(AuthContext)
+  // console.log(currentUser);
+  const userId = currentUser[0].ref._documentPath._parts[1]
+  // console.log(userId);
+  const [photo,setPhoto] = useState('')
+  const [hasPhoto, setHasPhoto] = useState(false)
+  const [photoBase64,setPhotoBase64] = useState('')
+
+  const imageGallery = ()=>{
+    const options={
+      maxHeight:400,
+      maxWidth:400,
+      includeBase64:true,
+    }
+    launchImageLibrary(options,res=>{
+      if(res.didCancel){
+        setHasPhoto(false)
+        setPhoto('');
+        setPhotoBase64('');
+      }else{
+        setPhoto(res.assets[0].uri);
+        setPhotoBase64(res.assets[0].base64);
+        setHasPhoto(true);
+        firestore().collection('users').doc(userId)
+        .update({image:res.assets[0].base64})
+        .then(() => {
+          console.log('User updated!');
+        });
+      }
+    })
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      {/* header */}
       <View style={styles.header}>
-          <View style={styles.headerBackButton}>
-          <ArrowLeft height={14} onPress={()=>navigation.goBack()}/>
-          </View>
-          <View style={styles.headerTitleContainer}>
-                <Text style={styles.headerTitleText}>Profil</Text>
-          </View>
-          <Gap height={20}/>
+       <View style={styles.headerBackButton}>
+        <ArrowLeft height={14} onPress={()=>navigation.goBack()}/>
+       </View>
+       <View style={styles.headerTitleContainer}>
+        <Text style={styles.headerTitleText}>Profil</Text>
+       </View>
+       <Gap height={20}/>
       </View>
 
-        <View style={styles.containerImage}>
-          <Image source={require('../../assets/images/fotoprofil.jpg')} style={styles.ImageStyle}/>
+      {!hasPhoto && currentUser[0]._data.image === "" &&
+        <View style={styles.addPhoto}>
+         <Text style={styles.addPhotoText}>Add Photo</Text>
+         <Camera width={30} style={{position:'absolute',bottom:0,right:-6}} onPress={imageGallery}/>
         </View>
+      }
+        {currentUser[0]._data.image !== "" &&
+          <View style={[styles.containerImage]}>
+           <Image source={{uri:`data:image/png;base64,${photoBase64 || currentUser[0]._data.image}`}} style={styles.ImageStyle}/>
+           <Camera width={30} style={{position:'absolute',bottom:26,right:155}} onPress={imageGallery}/>
+          </View>
+      }
       <View style={styles.cardContainer}>
           <Text style={styles.titleText}>Nama Lengkap</Text>
         <View style={styles.card}>
@@ -98,7 +138,20 @@ const styles = StyleSheet.create({
     borderRadius:43.5,
     resizeMode: 'stretch',
     alignItems: 'center',
-}
-
+  },
+  addPhoto: {
+    height: 90,
+    width: 90,
+    backgroundColor: '#E5E5E5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 90,
+  },
+  addPhotoText: {
+    fontSize: 12,
+    fontFamily: 'Inter',
+    maxWidth: 40,
+    textAlign: 'center',
+  }
 })
 export default ProfileCustomer
