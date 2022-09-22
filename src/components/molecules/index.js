ce54f2c6792c353c226388f531adb69424a1478d
PrinceSP <princepasombaran@gmail.com>
2022-09-22 13:14:54 +0800

    export request list component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -5,5 +5,6 @@ import DrawerContentCustomer from './drawerContentCustomer'
 import Header from './header'
 import WorkshopComponent from './workshopComp'
 import MapFinder from './maps'
+import RequestLists from './requestList'
 
-export {FeedbackForm,ModalSuccess,DrawerContent,Header,WorkshopComponent,DrawerContentCustomer,MapFinder}
+export {FeedbackForm,ModalSuccess,DrawerContent,Header,WorkshopComponent,DrawerContentCustomer,MapFinder,RequestLists}
