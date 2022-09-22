8aac4960f1db34fbdf7760492ff029a6510153cc
PrinceSP <princepasombaran@gmail.com>
2022-09-22 13:15:47 +0800

    add styling, modal, button and input

--- src/components/molecules/workshopComp/index.js
+++ src/components/molecules/workshopComp/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {View,Text,TouchableOpacity,Image} from 'react-native'
-import {Gap} from '../../atoms'
+import {View,Text,TouchableOpacity,Image,StyleSheet,Modal} from 'react-native'
+import {Gap,Button,Input} from '../../atoms'
+import {ModalSuccess} from '../successModal'
 
 const WorkshopComponent = ({desc='Online',onPress,namaBengkel,address,image="",...rest}) => {
+
   return (
     <TouchableOpacity onPress={onPress} {...rest}>
       <View style={{width:"100%",flexDirection:'row',padding:15,justifyContent:"space-between"}}>
@@ -20,5 +22,45 @@ const WorkshopComponent = ({desc='Online',onPress,namaBengkel,address,image="",.
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingHorizontal:15,
+        backgroundColor:"#fff"
+    },
+    textCaption:{
+        fontFamily:'Nunito-Bold',
+        fontWeight:'700',
+        fontSize: 36,
+        color:'#000000',
+        letterSpacing: 0.4
+    },
+    ImageStyle: {
+        padding: 10,
+        // marginTop: 40,
+        // marginBottom: 35,
+        height: 87,
+        width: 87,
+        borderRadius:43.5,
+        resizeMode: 'stretch',
+        alignItems: 'center',
+    },
+    bottomSheet:{
+      position:'absolute',
+      bottom:0,
+      right:0,
+      left:0,
+      borderTopLeftRadius:20,
+      borderTopRightRadius:20,
+      zIndex:99,
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 1,
+      shadowRadius:3.84,
+      elevation:10,
+      paddingHorizontal:16,
+      paddingVertical:16,
+    },
+})
 
 export default WorkshopComponent
