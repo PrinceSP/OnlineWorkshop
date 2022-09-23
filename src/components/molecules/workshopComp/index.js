7f11810610e6ad1409ce8a2a2d86a3c823ab529f
PrinceSP <princepasombaran@gmail.com>
2022-09-23 17:43:26 +0800

    add dynamic component when flag is different

--- src/components/molecules/workshopComp/index.js
+++ src/components/molecules/workshopComp/index.js
@@ -1,25 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {View,Text,TouchableOpacity,Image,StyleSheet,Modal} from 'react-native'
 import {Gap,Button,Input} from '../../atoms'
+import {ArrowLeft,MapPin,Warning} from '../../../assets'
 import {ModalSuccess} from '../successModal'
+import Animated from 'react-native-reanimated'
 
-const WorkshopComponent = ({desc='Online',onPress,namaBengkel,address,image="",...rest}) => {
-
+const WorkshopComponent = ({flag,desc='Online',onPress,namaBengkel,address,image="",problem,location,...rest}) => {
+  const [visible,setVisible] = useState(false)
   return (
-    <TouchableOpacity onPress={onPress} {...rest}>
-      <View style={{width:"100%",flexDirection:'row',padding:15,justifyContent:"space-between"}}>
-        {image==="" ? <View style={{width:80,height:82,backgroundColor:"#444",borderRadius:15}}/> : <Image style={{width:80,height:82,backgroundColor:"#444",borderRadius:15}} source={{uri:`data:image/png;base64,${image}`}}/>}
-        <View>
-          <Text style={{fontFamily:"Nunito-Bold",color:"#000"}}>{namaBengkel}</Text>
-          <Gap height={6}/>
-          <Text style={{fontFamily:"Nunito-Light",color:"#000"}}>{address}</Text>
-          <Gap height={3}/>
-          <View style={{width:278,height:1,backgroundColor:"#555"}}/>
-          <Gap height={3}/>
-          <Text style={{color:desc==="Online"?"#B3B553":"#777",fontFamily:"Nunito-Bold"}}>{desc}</Text>
+      flag === 'history' ? <View >
+        <Modal transparent visible={visible}>
+          <View style={{flex:1,backgroundColor:'rgba(0,0,0,0.5)'}}>
+            <TouchableOpacity style={{flex:1}} onPress={()=>setVisible(false)}/>
+            <Animated.View style={[styles.bottomSheet,{backgroundColor:"#fff",shadowColor:"#000000"}]}>
+              <Text style={{color:"#000",fontFamily:"Nunito-Bold",height:50,width:"100%",fontSize:18}}>{problem}</Text>
+                <Gap height={20}/>
+                <View style={{borderStyle:'dotted',borderColor:"rgba(0,0,0,0.4)",borderWidth:2}}/>
+                <Gap height={20}/>
+                <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+                  <View style={{flexDirection:'row',alignItems:'center'}}>
+                    <MapPin/>
+                    <Text style={{color:"#000",fontFamily:"Nunito-Bold",fontSize:18}}>{location}</Text>
+                  </View>
+                </View>
+                <Gap height={20}/>
+                <Text style={{color:"#B3B553",fontFamily:"Nunito-Bold",fontSize:18}}>Sedang menunggu konfirmasi</Text>
+                <Gap height={75}/>
+                <View>
+                  <Text style={{width:"100%",textAlign:'right',color:"#000",fontFamily:"Nunito-Light"}}>Tekan batal jika tidak jadi servis</Text>
+                  <Gap height={4}/>
+                  <Button style={styles.button} name='Batalkan' size = {24} weight = 'bold' color ='#fff'/>
+                </View>
+              </Animated.View>
+          </View>
+        </Modal>
+        <View style={{width:"100%",flexDirection:'row',padding:15,justifyContent:"space-between"}}>
+          {image==="" ? <View style={{width:80,height:82,backgroundColor:"#444",borderRadius:15}}/> : <Image style={{width:80,height:82,backgroundColor:"#444",borderRadius:15}} source={{uri:`data:image/png;base64,${image}`}}/>}
+          <View>
+            <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+              <Text style={{fontFamily:"Nunito-Bold",color:"#000"}}> {namaBengkel}</Text>
+              <Button name='Lihat' color='#3043F0' onPress={()=>setVisible(true)}/>
+            </View>
+            <Gap height={6}/>
+            <Text style={{fontFamily:"Nunito-Light",color:"#000"}}>{address}</Text>
+            <Gap height={3}/>
+            <View style={{width:278,height:1,backgroundColor:"#555"}}/>
+            <Gap height={3}/>
+            <Text style={{color:desc==="Online"?"#B3B553":"#777",fontFamily:"Nunito-Bold"}}>{desc}</Text>
+          </View>
         </View>
       </View>
-    </TouchableOpacity>
+      : <TouchableOpacity onPress={onPress} {...rest}>
+        <View style={{width:"100%",flexDirection:'row',padding:15,justifyContent:"space-between"}}>
+          {image==="" ? <View style={{width:80,height:82,backgroundColor:"#444",borderRadius:15}}/> : <Image style={{width:80,height:82,backgroundColor:"#444",borderRadius:15}} source={{uri:`data:image/png;base64,${image}`}}/>}
+          <View>
+            <Text style={{fontFamily:"Nunito-Bold",color:"#000"}}>{namaBengkel}</Text>
+            <Gap height={6}/>
+            <Text style={{fontFamily:"Nunito-Light",color:"#000"}}>{address}</Text>
+            <Gap height={3}/>
+            <View style={{width:278,height:1,backgroundColor:"#555"}}/>
+            <Gap height={3}/>
+            <Text style={{color:desc==="Online"?"#B3B553":"#777",fontFamily:"Nunito-Bold"}}>{desc}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
   )
 }
 
@@ -61,6 +105,15 @@ const styles = StyleSheet.create({
       paddingHorizontal:16,
       paddingVertical:16,
     },
+    button:{
+      height: 50,
+      width:"100%",
+      backgroundColor: '#5E6B73',
+      width:"100%",
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 15
+    }
 })
 
 export default WorkshopComponent
